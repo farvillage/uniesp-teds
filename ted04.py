3d90ee3c07cfd98175a6308d208ba9211edc56ec
Ygor Carvalho <ygorsoares2003@gmail.com>
2023-03-23 11:02:50 -0300

    atividade TED04

---
+++ ted04.py
@@ -0,0 +1,13 @@
+###TED04:
+##R1:
+
+VN = str("Insira um valor númerico (positivo ou negativo): ")
+print(VN)
+
+##R2:
+
+N = float(input("Quantas maçã(s) você deseja? "))
+if N < 12:
+    print("A(s) maçã(s) custa(m) R$1,30 cada")
+elif N >= 12:
+    print("As maçãs custam R$1,00 cada")
